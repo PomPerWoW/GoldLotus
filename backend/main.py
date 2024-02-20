6c8f8cd4550e2e2df8f7bec210f64a2b55bd65eb
porfai <popeachyyy@gmail.com>
2024-02-20 21:34:49 +0700

    add line

--- backend/main.py
+++ backend/main.py
@@ -5,20 +5,20 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-assets = Jinja2Templates(directory="../00-assets/")
-pages = Jinja2Templates(directory="../02-pages/")
+assets = Jinja2Templates(directory="../assets/")
+pages = Jinja2Templates(directory="../pages/")
 
 # Assets
 app.mount("/assets",
-          StaticFiles(directory="../00-assets"), name="assets-static")
+          StaticFiles(directory="../assets"), name="assets-static")
 
 # Global
 app.mount("/global-static",
-          StaticFiles(directory="../01-global"), name="global-static")
+          StaticFiles(directory="../global"), name="global-static")
 
 # Home
 app.mount("/main-static",
-          StaticFiles(directory="../02-pages/00-home"), name="main-static")
+          StaticFiles(directory="../pages/00-home"), name="main-static")
 
 
 @app.get("/", response_class=HTMLResponse, tags=["website"])
