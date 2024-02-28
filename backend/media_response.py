a3e0ad0fdbdd222a2af9e2bbc2fadf4a9c1e7167
b1ngoinho11 <101256148+b1ngoinho11@users.noreply.github.com>
2024-02-28 15:39:06 +0700

    optimized media_response for mp4

--- backend/media_response.py
+++ backend/media_response.py
@@ -1,6 +1,6 @@
 
 from fastapi import APIRouter, Request, Response
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, StreamingResponse
 import sys
 import os
 
@@ -15,16 +15,24 @@ from database import *
 async def get_media(request: Request, response: Response, mediaID: str):
     try:
         if os.path.exists(os.path.join("uploads", str(mediaID)) + ".png"):
-            file_path = os.path.join("uploads", str(mediaID)) + ".png"
+            return FileResponse(os.path.join("uploads", str(mediaID)) + ".png")
+        
         elif os.path.exists(os.path.join("uploads", str(mediaID)) + ".jpg"):
-            file_path = os.path.join("uploads", str(mediaID)) + ".jpg"
+            return FileResponse(os.path.join("uploads", str(mediaID)) + ".jpg")
+        
         elif os.path.exists(os.path.join("uploads", str(mediaID)) + ".jpeg"):
-            file_path = os.path.join("uploads", str(mediaID)) + ".jpeg"
+            return FileResponse(os.path.join("uploads", str(mediaID)) + ".jpeg")
+        
         elif os.path.exists(os.path.join("uploads", str(mediaID)) + ".MP4"):
-            file_path = os.path.join("uploads", str(mediaID)) + ".MP4"
+            video_size = os.path.join("uploads", str(mediaID)) + ".MP4".stat().st_size
+            def iter_file():
+                with open(os.path.join("uploads", str(mediaID)) + ".MP4", "rb") as video_file:
+                    while chunk := video_file.read(65536):
+                        yield chunk
+            return StreamingResponse(iter_file(), media_type="video/mp4", headers={"Accept-Ranges": "bytes", "Content-Length": str(video_size)})
+        
         else:
             raise Exception("File not found in the db.")
 
-        return FileResponse(file_path)
     except Exception as e:
         return {"detail": str(e)}
