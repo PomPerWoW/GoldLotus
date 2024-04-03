5e5f107a48bb7dca1a73ba61a15948f3ae83f999
b1ngoinho11 <budthbundit.thayaurai@gmail.com>
2024-04-03 23:31:19 +0700

    merge updated frontend & added home lastest blog

--- pages/00-home/pyscript.py
+++ pages/00-home/pyscript.py
@@ -1,17 +1,14 @@
-
 import js
 from pyodide.ffi import create_proxy
 from pyodide.http import pyfetch
+from datetime import datetime
 
 document = js.document
 
-latestBlog1 = document.getElementById("latestBlog1")
-latestAuthor1 = document.getElementById("latestAuthor1")
-latestDate1 = document.getElementById("latestDate1")
-
-latestBlog2 = document.getElementById("latestBlog2")
-latestAuthor2 = document.getElementById("latestAuthor2")
-latestDate2 = document.getElementById("latestDate2")
+latestBlog = [
+    [document.getElementById("latestBlog1"), document.getElementById("latestAuthor1"), document.getElementById("latestDate1")],
+    [document.getElementById("latestBlog2"), document.getElementById("latestAuthor2"), document.getElementById("latestDate2")]
+             ]
 
 async def getCurrentBlogID():
     try:
@@ -25,7 +22,7 @@ async def getCurrentBlogID():
             return data
     except Exception as e:
         print(e)
-        
+
 async def loadBlog(index):
     try:
         response = await pyfetch(
@@ -38,3 +35,38 @@ async def loadBlog(index):
             return data
     except Exception as e:
         print(e)
+
+async def getUserDetail(userID):
+    try:
+        response = await pyfetch(
+            url=f"/user/getUser?userId={userID}", 
+            method='GET',
+            headers={'Content-Type': 'application/json'}
+        )
+        if response.ok:
+            data = await response.json()
+            return data
+    except Exception as e:
+        print(e) 
+
+async def main():
+    current_blog_id = await getCurrentBlogID()
+    if current_blog_id:
+        current_blog_id = current_blog_id["currentBlogID"]
+        count = 0
+        while count < 2:
+            current_blog_id -= 1
+            blog_data = await loadBlog(current_blog_id)
+            if blog_data.get("detail"):
+                continue
+        
+            if blog_data:
+                username = await getUserDetail(blog_data["author"])
+                
+                latestBlog[count][0].innerText = blog_data["text"]
+                latestBlog[count][1].innerText = username.get("username")
+                latestBlog[count][2].innerText = datetime.fromisoformat(blog_data["timestamp"]).strftime("%Y-%m-%d %H:%M:%S")
+                count += 1
+
+import asyncio
+asyncio.ensure_future(main())
